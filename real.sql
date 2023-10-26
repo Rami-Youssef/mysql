0dd41fa5e45867109308791e51733ca102be3751
Rami-Youssef <ramiyoussefxd@gmail.com>
2023-10-26 20:28:33 +0200

    .

--- real.sql
+++ real.sql
@@ -1,3 +1,4 @@
+#ex1
 create database lts;
 use lts;
 create table Salarie (
@@ -24,4 +25,48 @@ CREATE table Charge (
     id_deplacement int,
     foreign key(id_deplacement) REFERENCES Deplacement(id_deplacement)
     );
+#2
+INSERT into salarie(nom, prenom , echelle, email, password)
+values("amine","bdu",8,"amine@gmail.com","amine");
+
+#3
+create view affiche_Salarie
+as select  nom, prenom , echelle,
+CASE 
+    when  echelle>9 then "Cadre"
+    when echelle between 6 and 9 then "Maitrise principale "
+    else "Maitrise"
+END
+as grade from Salarie;
+
+
+select * from affiche_Salarie;
+
+#4
+delimiter $
+create procedure ajouter_Deplacement(
+    p_date_debut date, 
+    p_date_fin date,
+    p_kilometrage int, 
+    p_matricule int
+)
+BEGIN
+declare v_date date;
+set v_date= DATEDIFF(p_date_fin,p_date_debut);
+if p_date_fin > p_date_debut and p_kilometrage>50 THEN
+    insert into Deplacement(date_debut, date_fin, kilometrage, matricule)
+    VALUES(p_date_debut, p_date_fin, p_kilometrage, p_matricule);
+else SELECT "nah bruh" as "error message";
+end if;
+end$
+call ajouter_Deplacement("20030101","20040101",20,2)
+
+#5
+delimiter $
+create procedure  ajouter_charge(p_libelle int, montant decimal(10,2), p_date_charge date, p_id_deplacement int)
+BEGIN
+
+declare v_echelle int;
+SELECT s.echelle into v_echelle from Salarie as s join Deplacement as D on s.matricule=D.matricule where id_deplacement=p_id_deplacement;
+
 
