a1cfb6d3c42e483c1db18e04774222c93884f66e
Rami-Youssef <ramiyoussefxd@gmail.com>
2023-11-01 09:51:52 +0100

    .

---
+++ playground-1.mongodb.js
@@ -0,0 +1,14 @@
+
+use ("ecole");
+const collection="etudiants";
+db.createCollection("etudiants");
+show dbs;
+show collections;
+db.etudiants.insertOne(
+    {
+        _id:1,
+        nom:"youssef",
+        age:16
+    }
+)
+db.etudiants.find()
