e392f738c60aaf93a1a312604aaf822092617ded
Rami-Youssef <ramiyoussefxd@gmail.com>
2023-11-01 10:05:18 +0100

    .

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,5 +1,5 @@
 
-use ("ecole");
+/*use ("ecole");
 const collection="etudiants";
 db.createCollection("etudiants");
 show dbs;
@@ -13,4 +13,51 @@ db.etudiants.insertOne(
 )
 db.etudiants.find()
 db.etudiants.drop()
-db.dropDatabase()
+db.dropDatabase()*/
+
+
+use article;
+db.createCollection("articles");
+db.articles.insertMany(
+    [
+        {
+            _id:1,
+            Désignation:"Clé USB 16 BG ",
+            Quantité:26,
+            Prix:250.50,
+            TVA:20,
+            Catégorie:"Informatique"
+        },
+        {
+            _id:2,
+            Désignation:"Clavier USB HP",
+            Quantité:12,
+            Prix:130.75,
+            TVA:25,
+            Catégorie:"Informatique"
+        },
+        {
+            _id:3,
+            Désignation:"Souris USB LG",
+            Quantité:25,
+            Prix:58.00,
+            TVA:30,
+            Catégorie:"Informatique"
+        },
+        {
+            _id:4,
+            Désignation:"Agenda 2024",
+            Quantité:56,
+            Prix:46.00,
+            TVA:46.00,
+            Catégorie:"Bureautique"
+        },{
+            _id:5,
+            Désignation:"Registre 400 pages",
+            Quantité:60,
+            Prix:80.50,
+            TVA:80.50,
+            Catégorie:"Bureautique"
+        }
+    ]
+)
