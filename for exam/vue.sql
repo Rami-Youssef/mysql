48b3a06d08d6c94346278ff0a30ee198b72c2b75
Rami-Youssef <ramiyoussefxd@gmail.com>
2023-11-20 21:11:05 +0100

    .

--- for exam/vue.sql
+++ for exam/vue.sql
@@ -29,4 +29,31 @@ SELECT * from viewarticle;
 ------------------------------------------------------------
 CREATE view vue2
 AS
-SELECT designation,libelle from Article as
+SELECT designation,libelle from Article as a join Categorie as C
+on a.id_categorie=C.id_categorie;
+
+SELECT * from vue2;
+--------------------------------------------------------------------
+
+create view vue3
+AS
+SELECT sum(reference) as nbr,libelle from Categorie
+as c left join  Article as a   on a.id_categorie=C.id_categorie;
+drop view vue3;
+SELECT * from vue3;
+--------------------------------------------------------------------
+create view vue4
+AS
+SELECT sum(qte_stock) as somme,avg(qte_stock) as average
+from Article as a join categorie as c on a.id_categorie=c.id_categorie
+GROUP BY c.id_categorie;
+SELECT * from vue4;
+--------------------------------------------------------------------
+create view vue7
+AS
+SELECT * from Article ORDER BY prix desc limit 1;
+SELECT * from vue7;
+--------------------------------------------------------------------
+ALTER view vue7 as select * from article order by prix asc limit 1;
+--------------------------------------------------------------------
+drop view vue7;
