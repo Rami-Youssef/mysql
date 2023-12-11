b9a5b51662e589d913e2bc8b2a6ee93f62700da1
Rami-Youssef <ramiyoussefxd@gmail.com>
2023-12-11 18:13:58 +0100

    d

---
+++ achraf/event.sql
@@ -0,0 +1,11 @@
+create event annualTask on schedule
+at current_timestamp( )+ interval '12' hour
+Do 
+BEGIN
+end
+"2025-02-11"
+
+create event annualTask2 on schedule
+starts current_timestamp() '12'
+Do 
+
